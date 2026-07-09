62c8c642a550fb89b1bc58da80d3ef85bfd50ab7
qinsiyuancn <130231069@qq.com>
2026-07-09 22:56:34 +0800

    about target speed segment
    Signed-off-by: qinsiyuancn <130231069@qq.com>

--- motion_analyzer.py
+++ motion_analyzer.py
@@ -7,18 +7,26 @@ import sys
 
 col_speed = 'speed'
 
-def calculate_tolerance(stable_speed_abs, target_speed_value):
-    if stable_speed_abs.empty:
-        raise ValueError('invailid value to calculate tolerace')
-    max_speed = stable_speed_abs.max()
-    temp_tolerance = abs(max_speed - target_speed_value) / target_speed_value
-    temp_lower_limit = target_speed_value - (target_speed_value * temp_tolerance)
+def get_upper_index(stable_speed_abs, lower_limit):
     above_limit_mask = stable_speed_abs > temp_lower_limit
     valid_indice = above_limit_mask[above_limit_mask].index
     if valid_indice.empty:
         raise ValueError("no stable(about equal target) speed data")
     first_idx = valid_indice.min()
     last_idx = valid_indice.max()
+    return first_idx, last_idx
+
+def get_target_speed_segment_index(stable_speed_abs, target_speed_value, tolerance):
+    lower_limit = target_speed_value - (target_speed_value * tolerance)
+    return get_upper_index(stable_speed_abs, lower_limit)
+
+
+def calculate_tolerance(stable_speed_abs, target_speed_value):
+    if stable_speed_abs.empty:
+        raise ValueError('invailid value to calculate tolerace')
+    max_speed = stable_speed_abs.max()
+    temp_tolerance = abs(max_speed - target_speed_value) / target_speed_value
+    first_idx, last_idx = get_target_speed_segment_index(stable_speed_abs, target_speed_value, temp_tolerance)
     temp_segment = stable_speed_abs.loc[first_idx:last_idx]
     min_speed_in_temp = temp_segment.min()
     min_tolerance = abs(target_speed_value - min_speed_in_temp) / target_speed_value
@@ -61,12 +69,14 @@ if __name__ == "__main__":
         df['segment'] = (df['speed_main_dir'] != df['speed_main_dir'].shift()).cumsum()
         segment_length = df[ df['speed_main_dir'] != 0 ].groupby('segment').size()
         if segment_length.empty:
-            df['speed_stable'] = 0
-        else:
-            longest_segment = segment_length.idxmax()
-            df['speed_stable'] = df.apply( lambda row : row['speed_main_dir'] if(row['segment'] == longest_segment and row['speed_main_dir'] != 0) else 0, axis = 1)
-            df['speed_stable_abs'] = df['speed_stable'].abs()
-            tolerance = calculate_tolerance(df['speed_stable_abs'], target_speed_value)
+            raise ValueError(f"no stable speed segment");
+        longest_segment = segment_length.idxmax()
+        df['speed_stable'] = df.apply( lambda row : row['speed_main_dir'] if(row['segment'] == longest_segment and row['speed_main_dir'] != 0) else 0, axis = 1)
+        df['speed_stable_abs'] = df['speed_stable'].abs()
+        df['target_speed_segment'] = 0
+        tolerance = calculate_tolerance(df['speed_stable_abs'], target_speed_value)
+        first_idx, last_idx = get_target_speed_segment_index(df['speed_stable_abs'], target_speed_value, tolerance)
+        df.loc[first_idx:last_idx, 'target_speed_segment'] = target_speed
 
     except Exception as e:
         print(f"failed: {e}")
